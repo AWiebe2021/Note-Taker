061930f4a3c2ce5feffe13cce605c63d32e2fd2b
AWiebe2021 <85322039+AWiebe2021@users.noreply.github.com>
2021-08-26 12:49:12 -0700

    Add Heroku

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./db/');
-const htmlRoutes = require('./public/');
+// const apiRoutes = require('./Develop/db');
+const htmlRoutes = require('./Develop/routes/');
+const { notes } = require('./Develop/db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('./Develop/public'));
 
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
 
-app.use('/api/notes', apiRoutes);
-app.use('/notes', htmlRoutes);
+// app.use('/api/notes', apiRoutes);
+// app.use('/notes', htmlRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
